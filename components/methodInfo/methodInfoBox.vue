b894e17628373ee177b4d734e95d6dc21af013a6
Naz Sharid <post@nazs.net>
2021-04-24 08:07:03 +0100

    added methodInfo 2

--- components/methodInfo/methodInfoBox.vue
+++ components/methodInfo/methodInfoBox.vue
@@ -2,13 +2,13 @@
   <one-section>
     <h3>Remote Steps</h3>
     <div class="devicePasswordNew boxes">
-      {{ customer }}
-      {{ $store.state.customers }}
-      <!-- <new-method
-        v-for="method in customer[0].methodInfo"
+      <!-- {{ customer }} -->
+      <!-- {{ $store.state.customers }} -->
+      <new-method
+        v-for="method in $store.state.customers[0].methodInfo"
         :key="method._id"
         :method="method"
-      > -->
+      >
       </new-method>
     </div>
     <nuxt-link to="/add-method"><Plus /></nuxt-link>
