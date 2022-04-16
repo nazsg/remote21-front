83d78c32efbc358c4044c5fe9025952f06cf68c4
Nazie Sharid <post@nazs.net>
2022-04-16 07:53:00 +0100

    added form validation including url check

--- pages/add-method.vue
+++ pages/add-method.vue
@@ -20,8 +20,14 @@
           <input v-model="password" type="text" />
         </li>
         <label for="item">Notes:</label>
-        <textarea id="" v-model="notes" name="" cols="30" rows="10"></textarea>
+        <textarea id="" v-model="notes" name="" cols="30" rows="5"></textarea>
       </ul>
+      <template v-if="errors.length > 0">
+        <ul class="formErrors">
+          Required
+          <li v-for="(err, i) in errors" :key="i">{{ err }}</li>
+        </ul>
+      </template>
       <div slot="default" class="actions">
         <div @click="back">
           <Button>
@@ -35,7 +41,7 @@
             <span class="icon iconRight orange"><DeleteForever /></span>
           </Button>
         </div>
-        <div @click="insertStep">
+        <div @click="validate">
           <Button>
             <span class="text textLeft orange">Add server</span>
             <span class="icon iconRight orange"><SendIcon /></span>
@@ -54,11 +60,12 @@ export default {
     return {
       customer: '',
       name: '',
-      url: [],
+      url: '',
       username: '',
       password: '',
       notes: '',
       id: '',
+      errors: [],
     }
   },
   mounted() {
@@ -76,6 +83,26 @@ export default {
       this.url = ''
       this.username = ''
       this.password = ''
+      this.notes = ''
+      this.errors = []
+    },
+    validate() {
+      this.errors = []
+      const reg =
+        /https?:\/\/w{0,3}\w*?\.(\w*?\.)?\w{2,3}\S*|\.(\w*?\.)?\w*?\.\w{2,3}\S*|(\w*?\.)?\w*?\.\w{2,3}[/?]\S*/
+      // reg.test('www.google.com')    # will return true
+      if (this.name.trim() === '') this.errors.push('method name')
+      if (this.url.trim() !== '') {
+        if (this.url.indexOf('http://') !== 0)
+          this.url = 'http://' + this.url.trim()
+        if (!reg.test(this.url.trim())) this.errors.push('inavlid url')
+      } else {
+        this.errors.push('url')
+      }
+      if (this.username.trim() === '') this.errors.push('username')
+      if (this.password.trim() === '') this.errors.push('password')
+      if (this.errors.length > 0) return
+      this.insertStep()
     },
     insertStep(serverId, viewMode, editMode) {
       const token = localStorage.getItem('token')
